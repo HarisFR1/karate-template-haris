9bfc026d3cb6dd98a800304ec49a1f0fd34714fa
haris <harisfahim6@gmail.com>
2023-08-22 18:16:42 -0400

    initial changes

--- src/test/java/examples/users/UsersRunner.java
+++ src/test/java/examples/users/UsersRunner.java
@@ -6,7 +6,7 @@ class UsersRunner {
     
     @Karate.Test
     Karate testUsers() {
-        return Karate.run("PlanCode").relativeTo(getClass());
+        return Karate.run("GenerateToken").relativeTo(getClass());
     }    
 
 }
